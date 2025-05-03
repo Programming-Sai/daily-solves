fa73aefba88d6874245f85f6ba5902ac3d298294
Programming-Sai <saiahprog6@gmail.com>
2025-05-03 12:00:04 +0000

    Auto commit at 2025-05-03 12:00:03

--- A2SV Ghana G6 - Round #10/B_Kuriyama_Mirai_s_Stones.py
+++ A2SV Ghana G6 - Round #10/B_Kuriyama_Mirai_s_Stones.py
@@ -1,8 +1,27 @@
 from fprintx import printx
 
+from itertools import accumulate
 
 def check(n, v, m, q):
-    pass
+    sorted_stones = sorted(v)
+    # print(sorted_stones)
+
+    prefix_sum_normal = list(accumulate(v))
+    prefix_sum_sorted = list(accumulate(sorted_stones))
+    types = [
+        prefix_sum_normal,
+        prefix_sum_sorted
+    ]
+
+    for type_, l, r in q:
+        arr = types[type_ - 1]
+        # printx(arr, arr[r-1], arr[l-2])
+        if l > 1:
+            print(arr[r-1] - arr[l-2])
+        else:
+            print(arr[r-1])
+    
+
 
 
 n = int(input())
--- A2SV Ghana G6 - Round #10/E_Good_Subarrays.py
+++ A2SV Ghana G6 - Round #10/E_Good_Subarrays.py
@@ -1,29 +1,57 @@
 from fprintx import printx
 
+
+
+from itertools import accumulate
 from collections import defaultdict
 
 
 def check(n, a):
-    sum_hash = defaultdict(int)
+    # sum_hash = defaultdict(int)
+    # sum_hash[0] = 1
+    # prefix_sum = list(accumulate(a))
+    # curr_sum = 0
+    # result_count = 0
+    # l = r = 0
+    # for r in range(n):
+    #     curr_sum = prefix_sum[r]
+    #     # if curr_sum == (r-l+1):
+    #         # printx(curr_sum)
+    #     result_count += sum_hash[curr_sum]
+    #         # l += 1
+
+    #     sum_hash[curr_sum] += 1
+    #     # r += 1
+    #     printx(sum_hash, l, r, curr_sum, a[l:r], widths=[15])
+
+    # return result_count
+
+    result_sum = 0
     curr_sum = 0
-    result_count = 0
-    l = r = 0
-    while r < n:
+    for r in range(n):
         curr_sum += a[r]
-        if curr_sum == (r-l+1):
-            # printx(curr_sum)
-            result_count += sum_hash[curr_sum]
-            l += 1
 
-        sum_hash[curr_sum] += 1
-        r += 1
-        printx(sum_hash, l, r, curr_sum, a[l:r], widths=[15])
+        if a[r] == 1:
+            result_sum += 1
 
-    return result_count
+        if curr_sum == (r+1):
+            print(a[:r], r)
+            result_sum += 1
+    curr_sum = 0
+    for l in range(n-1, -1, -1):
+        curr_sum +=  a[l]
+        if curr_sum == l + 1:
+            # print(l, "-")
+            print(a[l:], l)
+            result_sum += 1
+    
+    return result_sum
 
 
 n = 3
 a = [1, 2, 0]
+# n = 5
+# a = [1, 1, 0, 1, 1]
 print(check(n, a))
 
 
