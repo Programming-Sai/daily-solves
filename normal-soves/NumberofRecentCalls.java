699fd85408fe88376e998ec838198b7c606da706
Programming-Sai <saiahprog6@gmail.com>
2025-05-03 09:20:52 +0000

    Auto commit at 2025-05-03 09:20:51

--- normal-soves/NumberofRecentCalls.java
+++ normal-soves/NumberofRecentCalls.java
@@ -1,15 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class NumberofRecentCalls {
 
-    public NumberofRecentCalls() {
+    Deque<Integer> callDeque;
 
+    public NumberofRecentCalls() {
+        callDeque = new ArrayDeque<>();
     }
 
     public int ping(int t) {
-
-        return 0;
+        // System.out.println(callDeque);
+        while (!callDeque.isEmpty() && (t - 3000) > callDeque.peekFirst()) {
+            callDeque.pop();
+        }
+        callDeque.add(t);
+        return callDeque.size();
     }
 
     public static void main(String[] args) {
-
+        NumberofRecentCalls recentCounter = new NumberofRecentCalls();
+        System.out.println(recentCounter.ping(1)); // Output: 1
+        System.out.println(recentCounter.ping(100)); // Output: 2
+        System.out.println(recentCounter.ping(3001)); // Output: 3
+        System.out.println(recentCounter.ping(3002)); // Output: 3
     }
 }
