ba1f22a6e54d7084befabb7635233788f24452d4
Programming-Sai <saiahprog6@gmail.com>
2025-05-05 12:10:55 +0000

    Auto commit at 2025-05-05 12:10:54

--- A2SV Ghana G6 - Round #10/E_Good_Subarrays.py
+++ A2SV Ghana G6 - Round #10/E_Good_Subarrays.py
@@ -7,46 +7,44 @@ from collections import defaultdict
 
 
 def check(n, a):
-    # sum_hash = defaultdict(int)
-    # sum_hash[0] = 1
+    sum_hash = defaultdict(int)
+    sum_hash[0] = 1
     # prefix_sum = list(accumulate(a))
-    # curr_sum = 0
-    # result_count = 0
-    # l = r = 0
-    # for r in range(n):
-    #     curr_sum = prefix_sum[r]
-    #     # if curr_sum == (r-l+1):
-    #         # printx(curr_sum)
-    #     result_count += sum_hash[curr_sum]
-    #         # l += 1
-
-    #     sum_hash[curr_sum] += 1
-    #     # r += 1
-    #     printx(sum_hash, l, r, curr_sum, a[l:r], widths=[15])
-
-    # return result_count
-
-    result_sum = 0
     curr_sum = 0
+    result_count = 0
     for r in range(n):
-        curr_sum += a[r]
+        curr_sum = a[r]
+        # if curr_sum == (r-l+1):
+            # printx(curr_sum)
+        result_count += sum_hash[curr_sum - 6]
+            # l += 1
 
-        if a[r] == 1:
-            result_sum += 1
+        sum_hash[curr_sum] += 1
+        # r += 1
 
-        if curr_sum == (r+1):
-            print(a[:r], r)
-            result_sum += 1
+    return result_count
 
-    curr_sum = 0
-    for l in range(n-1, -1, -1):
-        curr_sum +=  a[l]
-        if curr_sum == l + 1:
-            # print(l, "-")
-            print(a[l:], l)
-            result_sum += 1
+    # result_sum = 0
+    # curr_sum = 0
+    # for r in range(n):
+    #     curr_sum += a[r]
+
+    #     if a[r] == 1:
+    #         result_sum += 1
+
+    #     if curr_sum == (r+1):
+    #         print(a[:r], r)
+    #         result_sum += 1
+
+    # curr_sum = 0
+    # for l in range(n-1, -1, -1):
+    #     curr_sum +=  a[l]
+    #     if curr_sum == l + 1:
+    #         # print(l, "-")
+    #         print(a[l:], l)
+    #         result_sum += 1
     
-    return result_sum
+    # return result_sum
 
 
 # n = 3
