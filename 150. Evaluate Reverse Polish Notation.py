3399f2d4680c6f7109cd035305cbf9597ed167d0
Programming-Sai <saiahprog6@gmail.com>
2025-05-06 16:00:03 +0000

    Auto commit at 2025-05-06 16:00:03

--- 150. Evaluate Reverse Polish Notation.py
+++ 150. Evaluate Reverse Polish Notation.py
@@ -3,13 +3,17 @@ from fprintx import printx
 
 
 def add(a, b):
+    print(a, "+", b)
     return a+b
 def sub(a, b):
-    return a-b
+    print(a, "-", b)
+    return int(a)-int(b)
 def multipy(a, b):
+    print(a, "*", b)
     return a*b
 def div(a, b):
-    return a//b
+    print(a, "/", b)
+    return int(a / b) if a * b > 0 else -(abs(a) // abs(b))
 def evalRPN(tokens):
     stack = []
     opeartions_set = {'+', '-', '/', '*'}
@@ -21,13 +25,14 @@ def evalRPN(tokens):
     }
     for token in tokens:
         if token not in opeartions_set:
-            stack.append(token)
+            stack.append(int(token))
         else:
             if len(stack) > 1:
-                stack.append(lambda b = stack.pop(), a = stack.pop() : operations[token](a, b))
-    return stack
+                stack.append((lambda b=stack.pop(), a=stack.pop(): operations[token](a, b))())
+        print(stack)
+    return stack[0]
             
 
-print(evalRPN( tokens = ["2","1","+","3","*"]))
+# print(evalRPN( tokens = ["2","1","+","3","*"]))
 # print(evalRPN( tokens = ["4","13","5","/","+"]))
-# print(evalRPN( tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
+print(evalRPN( tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
