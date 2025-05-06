51a4f7db928f97ee17735a7140d5b555b58e12ed
Programming-Sai <saiahprog6@gmail.com>
2025-05-06 15:00:02 +0000

    Auto commit at 2025-05-06 15:00:02

---
+++ 150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,33 @@
+from fprintx import printx
+
+
+
+def add(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def multipy(a, b):
+    return a*b
+def div(a, b):
+    return a//b
+def evalRPN(tokens):
+    stack = []
+    opeartions_set = {'+', '-', '/', '*'}
+    operations = {
+        '+': add,
+        '-': sub,
+        '*': multipy,
+        '/': div
+    }
+    for token in tokens:
+        if token not in opeartions_set:
+            stack.append(token)
+        else:
+            if len(stack) > 1:
+                stack.append(lambda b = stack.pop(), a = stack.pop() : operations[token](a, b))
+    return stack
+            
+
+print(evalRPN( tokens = ["2","1","+","3","*"]))
+# print(evalRPN( tokens = ["4","13","5","/","+"]))
+# print(evalRPN( tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
--- A_Two_Frogs.py
+++ A_Two_Frogs.py
@@ -7,29 +7,43 @@ def check(n, a, b):
     winner = None
     turn = 'a'
     while True:
+        print(['a' if i == a else 'b' if i == b else "_" for i in range(1, n+1)])
+
+
         if turn == 'a':
-            if a > b and (a-1) > 0 and (a-1) != b:
+            if (a > b and (a-1) > 0 and (a-1) != b) or (a+1 == b and a-1 > 0):
                 a -= 1
-            elif a < b and (a + 1) < n+1 and (a+1) != b:
+            elif (a < b and (a + 1) < n+1 and (a+1) != b) or (a-1 == b and a+1 < n+1):
                 a += 1
-        
+
+
+        print(['a' if i == a else 'b' if i == b else "_" for i in range(1, n+1)])
         turn  = 'b'
+
+
         if turn  == 'b':
-            if b > a and (b-1) > 0 and (b-1) != a:
+            if (b > a and (b-1) > 0 and (b-1) != a) or (b+1 == a and b-1 > 0):
                 b -= 1
-            elif b < a and (b + 1) < n+1 and (b+1) != a:
+            if( b < a and (b + 1) < n+1 and (b+1) != a) or (b-1 == a and b+1 < n+1):
                 b += 1
+
+
+
+        print(['a' if i == a else 'b' if i == b else "_" for i in range(1, n+1)])
         turn  = 'a'
 
-        if 0 < a < b or b < a < n+1:
+        if (0 < a < b or b < a < n+1) and (a == b+1 or a == b-1):
             winner = 'b'
+            print(0, a, b, n+1)
             return 'NO'
-        elif 0 < b < a or a < b < n+1:
+        elif (0 < b < a or a < b < n+1) and (b == a+1 or b == a-1):
             winner = 'a'
+            print(0, a, b, n+1)
             return 'YES'
         
 
-n, a, b = 2, 1, 2
+# n, a, b = 2, 1, 2
+n, a, b = 5, 2, 4
 print(check(n, a, b))
 
 
