ee943d04471397702b9affde131f044ca311185b
Programming-Sai <saiahprog6@gmail.com>
2025-05-07 12:00:03 +0000

    Auto commit at 2025-05-07 12:00:02

--- normal-soves/LinkedListCycleII.java
+++ normal-soves/LinkedListCycleII.java
@@ -16,7 +16,37 @@ class ListNode {
 }
 
 public class LinkedListCycleII {
-    public ListNode detectCycle(ListNode head) {
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
 
+        ListNode t = head, r = head;
+        while (r != null && r.next != null) {
+            t = t.next;
+            r = r.next.next;
+            if (r == t) {
+                break;
+            }
+        }
+        if (r == null && r.next == null) {
+            return null;
+        }
+
+        t = head;
+        while (r != t) {
+            t = t.next;
+            r = r.next;
+        }
+        return t;
+    }
+
+    public static void main(String[] args) {
+        // [3,2,0,-4]
+        ListNode head = new ListNode(3, new ListNode(2));
+        ListNode cy = head.next;
+        head.next.next = new ListNode(0, new ListNode(-4, cy));
+
+        System.out.println(detectCycle(head).val);
     }
 }
