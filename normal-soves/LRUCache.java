f24819f284b130e702d73bbdb36591efccf1b413
Programming-Sai <saiahprog6@gmail.com>
2025-05-07 13:00:05 +0000

    Auto commit at 2025-05-07 13:00:04

---
+++ normal-soves/LRUCache.java
@@ -0,0 +1,43 @@
+
+class ListNode {
+    int val;
+    ListNode next, prev;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    ListNode(int val, ListNode prev) {
+        this.val = val;
+        this.prev = prev;
+    }
+
+    ListNode(int val, ListNode next, ListNode prev) {
+        this.val = val;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+public class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
