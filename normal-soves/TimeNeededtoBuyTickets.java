4e6b3b4c9559987ba496fa0e81449b8deece5deb
Programming-Sai <saiahprog6@gmail.com>
2025-05-08 15:00:02 +0000

    Auto commit at 2025-05-08 15:00:02

--- normal-soves/TimeNeededtoBuyTickets.java
+++ normal-soves/TimeNeededtoBuyTickets.java
@@ -1,3 +1,39 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+
 public class TimeNeededtoBuyTickets {
 
+    public static int timeRequiredToBuy(int[] tickets, int k) {
+        int n = tickets.length;
+        int i = -1;
+        int c = 0;
+        System.out.println(Arrays.toString(tickets));
+        while (true) {
+            i++;
+            i = (i % n);
+
+            if (tickets[i] == 0) {
+                continue;
+            }
+            tickets[i]--;
+            System.out.println(Arrays.toString(tickets));
+            c++;
+
+            if (i == k) {
+                if (tickets[i] == 0) {
+                    break;
+                }
+            }
+
+        }
+
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(timeRequiredToBuy(new int[] { 2, 3, 2 }, 2));
+        // System.out.println(timeRequiredToBuy(new int[] { 5, 1, 1, 1 }, 0));
+    }
 }
