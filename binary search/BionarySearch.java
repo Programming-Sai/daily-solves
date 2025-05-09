98edb91af667b26b3caeffa0bcd94511716559dc
Programming-Sai <saiahprog6@gmail.com>
2025-05-09 12:10:17 +0000

    Auto commit at 2025-05-09 12:10:17

--- binary search/BionarySearch.java
+++ binary search/BionarySearch.java
@@ -1,18 +1,30 @@
 public class BionarySearch {
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int target = 9;
+        int target = 1;
         int l = 0;
-        int r = 9;
-
-        while (l < r) {
-            int mid = l / r;
+        int r = arr.length - 1;
+        int numberOfOperations = 0;
+        boolean found = false;
+        int mid;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            System.out.println(l + ", " + mid + ", " + r);
             if (arr[mid] == target) {
                 System.out.println(target);
+                found = true;
                 break;
             } else if (arr[mid] < target) {
-                r = mid;
+                l = mid + 1;
+            } else if (arr[mid] > target) {
+                r = mid - 1;
             }
+            numberOfOperations++;
+        }
+        if (found) {
+            System.out.println("Target " + target + " found in " + numberOfOperations + " operations.");
+        } else {
+            System.out.println("Target " + target + " not found.");
         }
     }
 }
