645d9cfafe7c7bf51eacf3f93db3708d3bacb782
Programming-Sai <saiahprog6@gmail.com>
2025-05-10 12:23:16 +0000

    Auto commit at 2025-05-10 12:23:15

--- A2SV Ghana G6 - Round #11/C_Make_Product_Equal_One.py
+++ A2SV Ghana G6 - Round #11/C_Make_Product_Equal_One.py
@@ -1,12 +1,30 @@
 from fprintx import printx
 
 def check(n, a):
-    a.sort()
-    number_of_negatives = i = 0
-    while a[i] < 0:
-        number_of_negatives += 1
-        i+=1
-    print(a, number_of_negatives)
+    # try:
+        a.sort()
+        number_of_negatives = i = 0
+        while i < n and a[i] < 0:
+            # if a[i] == -1:
+                # continue
+            number_of_negatives += 1
+            i+=1
+        
+        has_zero = 0 in set(a)
+
+        if number_of_negatives % 2:
+            if not has_zero:
+                number_of_negatives -= 1
+        result = 0
+        for i in range(n):
+            if i < number_of_negatives:
+                result += (-(a[i]))-1
+            else:
+                result += abs(1-a[i])
+        return result
+    # except Exception as e:
+        # return str(e)
+
 
 
 n = int(input())
