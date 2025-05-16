5531505dd19300b8e9084c7e9e424683ab5535e5
Programming-Sai <saiahprog6@gmail.com>
2025-05-16 02:00:04 +0000

    Auto commit at 2025-05-16 02:00:03

--- binary search/FindMinimumInRotatedSortedArray.java
+++ binary search/FindMinimumInRotatedSortedArray.java
@@ -1,6 +1,20 @@
 public class FindMinimumInRotatedSortedArray {
     public int findMin(int[] nums) {
-
+        int l, r, mid, n = nums.length;
+        l = 0;
+        r = n - 1;
+        if (nums[l] <= nums[r]) {
+            return nums[l];
+        }
+        while (l < r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[l];
     }
 
 }
