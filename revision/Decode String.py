30271ce984d6113c1845898c7c667ec98f2a0656
Programming-Sai <saiahprog6@gmail.com>
2025-05-21 13:00:03 +0000

    Auto commit at 2025-05-21 13:00:02

--- revision/Decode String.py
+++ revision/Decode String.py
@@ -1,11 +1,28 @@
 from fprintx import printx
 
 
-def decodeString(self, s: str) -> str:
-    pass
+def decodeString(s: str) -> str:
+    n = len(s)
+    result = []
+    num_stack = []
+    str_stack = []
+    i = n-1
+    while i > 0:
+        if s[i] == ']':
+            j = i
+            while True:
+                if s[j] == '[':
+                    break
+                # if s[j] == ']':
+                    # decodeString()
+                num_stack.append(s[j])
+            i = j
+            
+            
+
 
 
 print(decodeString(s = "3[a]2[bc]"))
-print(decodeString(s = "3[a2[c]]"))
-print(decodeString(s = "2[abc]3[cd]ef"))
+# print(decodeString(s = "3[a2[c]]"))
+# print(decodeString(s = "2[abc]3[cd]ef"))
 
