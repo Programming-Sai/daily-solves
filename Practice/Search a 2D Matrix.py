9b6668822547010b0b52ad5b1e31b241fc6dea0f
Programming-Sai <saiahprog6@gmail.com>
2025-06-02 09:01:01 +0000

    Auto commit at 2025-06-02 09:01:01

--- Practice/Search a 2D Matrix.py
+++ Practice/Search a 2D Matrix.py
@@ -2,13 +2,17 @@ from fprintx import printx
 
 
 def check(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
     m = len(matrix)
     n = len(matrix[0])
     l = 0
     r = m*n-1
 
     while l <= r:
-        mid = l+ (r-l)//2, row = mid // n, col = mid % n
+        mid = l + (r-l)//2
+        row = mid // n
+        col = mid % n
         if matrix[row][col] == target:
             return True
         elif matrix[row][col] > target:
@@ -36,3 +40,7 @@ print(check(matrix=[
 print(check(matrix=[
   [1]
 ], target=1))  # Expected: True
+
+
+print(check([], target=1))  # should be False
+print(check([[]], target=1))  # should also be False
