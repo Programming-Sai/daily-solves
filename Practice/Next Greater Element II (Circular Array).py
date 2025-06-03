12693d5687c87f432f5271ebd52f079ac07fb807
Programming-Sai <saiahprog6@gmail.com>
2025-06-03 12:08:37 +0000

    Auto commit at 2025-06-03 12:08:36

--- Practice/Next Greater Element II (Circular Array).py
+++ Practice/Next Greater Element II (Circular Array).py
@@ -14,8 +14,9 @@ def check(nums: List[int]) -> List[int]:
     return result
 
 
-print(check([1, 2, 1]))  # Expected: [2, -1, 2]
+print(check([1, 2, 1]))  # Expected: [2, -1, 2] 
 print(check([1, 2, 3, 4, 3]))  # Expected: [2, 3, 4, -1, 4]
 print(check([3, 8, 4, 1, 2]))  # Expected: [8, -1, 8, 2, 3]
 print(check([]))  # Expected: []
 print(check([2]))  # Expected: [-1]
+print(check([2, 2, 2, 2, 2]))  # Expected: [-1, -1, -1, -1, -1]
