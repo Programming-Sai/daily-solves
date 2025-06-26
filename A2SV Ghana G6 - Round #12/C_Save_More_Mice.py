c9e2d9ea20acf267a4dc55d401425f1b2558bc9d
Programming-Sai <saiahprog6@gmail.com>
2025-06-26 13:00:03 +0000

    Auto commit at 2025-06-26 13:00:02

--- A2SV Ghana G6 - Round #12/C_Save_More_Mice.py
+++ A2SV Ghana G6 - Round #12/C_Save_More_Mice.py
@@ -6,10 +6,9 @@ def check(n, k, x):
     x.sort()
     cat = save = 0
     for i in range(len(x)-1, -1, -1):
-        if x[i] < cat:
-            break
-        save += 1
-        cat += n - x[i]
+        if cat + n-x[i] < n:
+            save += 1
+            cat += n - x[i]
     return save
 
 
@@ -20,5 +19,5 @@ n, k, x = 10, 6, list(set([8, 7, 5, 4, 9, 4]))
 
 for _ in range(int(input())):
     n, k = map(int, input().split())
-    x = list(set(map(int, input().split())))
+    x = list(map(int, input().split()))
     print(check(n, k, x))
