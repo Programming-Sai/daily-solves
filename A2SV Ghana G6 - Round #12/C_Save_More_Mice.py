56dcd3c8fd9791f1a820eba3e10021a62d8345e7
Programming-Sai <saiahprog6@gmail.com>
2025-06-26 16:00:06 +0000

    Auto commit at 2025-06-26 16:00:05

--- A2SV Ghana G6 - Round #12/C_Save_More_Mice.py
+++ A2SV Ghana G6 - Round #12/C_Save_More_Mice.py
@@ -28,3 +28,30 @@ print(check(n, k, x))
 #     n, k = map(int, input().split())
 #     x = list(set(map(int, input().split())))
 #     print(check(n, k, x))
+
+
+
+
+
+# from fprintx import printx
+
+
+# def check(n, k, x):
+#     x.sort()
+#     cat = save = 0
+#     for i in range(len(x)-1, -1, -1):
+#         if cat + n-x[i] < n:
+#             save += 1
+#             cat += n - x[i]
+#     return save
+
+
+# n, k, x = 10, 6, list(set([8, 7, 5, 4, 9, 4]))
+# # n, k, x = 2, 8,list(set( [1]*8))
+# # print(check(n, k, x))
+
+
+# for _ in range(int(input())):
+#     n, k = map(int, input().split())
+#     x = list(map(int, input().split()))
+#     print(check(n, k, x))
