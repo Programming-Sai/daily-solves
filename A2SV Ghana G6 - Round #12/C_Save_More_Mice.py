e647791d31be927bc57bbc23ba4cd3502a398f8c
Programming-Sai <saiahprog6@gmail.com>
2025-06-26 14:00:05 +0000

    Auto commit at 2025-06-26 14:00:04

--- A2SV Ghana G6 - Round #12/C_Save_More_Mice.py
+++ A2SV Ghana G6 - Round #12/C_Save_More_Mice.py
@@ -4,20 +4,27 @@ from fprintx import printx
 
 def check(n, k, x):
     x.sort()
-    cat = save = 0
-    for i in range(len(x)-1, -1, -1):
-        if cat + n-x[i] < n:
+    l = cat = save = 0
+    r = len(x)-1
+    while l <= r:
+        printx(l, r, cat, save, x, widths=[3])
+        x[r] += 1
+        if x[r] == n:
+            r -= 1
             save += 1
-            cat += n - x[i]
+        cat += 1
+        if x[l] == cat:
+            l += 1
+            
     return save
 
 
 n, k, x = 10, 6, list(set([8, 7, 5, 4, 9, 4]))
 # n, k, x = 2, 8,list(set( [1]*8))
-# print(check(n, k, x))
+print(check(n, k, x))
 
 
-for _ in range(int(input())):
-    n, k = map(int, input().split())
-    x = list(map(int, input().split()))
-    print(check(n, k, x))
+# for _ in range(int(input())):
+#     n, k = map(int, input().split())
+#     x = list(set(map(int, input().split())))
+#     print(check(n, k, x))
