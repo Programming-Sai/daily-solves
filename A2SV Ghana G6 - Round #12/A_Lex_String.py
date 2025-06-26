58ad247bf7a725d750ba6b8be0c25f8a9b329361
Programming-Sai <saiahprog6@gmail.com>
2025-06-26 11:00:03 +0000

    Auto commit at 2025-06-26 11:00:02

--- A2SV Ghana G6 - Round #12/A_Lex_String.py
+++ A2SV Ghana G6 - Round #12/A_Lex_String.py
@@ -7,33 +7,35 @@ def check(n, m, k, a, b):
     c = ""
     # printx(n, m, k, a, b, widths=[3])
     range_n = min(n, m)
-    i = op_count = j = 0
-    print(a[0] < b[0])
-    while True:
-        if j > range_n or i > range_n: break
-        if op_count >= k: 
-            i, j = j, i
-            op_count = 0
-        printx(i, j, op_count)
-        if a[i] < b[j]:
+    i = ca = cb = j = 0
+    # print(a[0] < b[0], range_n)
+    while i < n and j < m:
+        if (a[i] < b[j] and ca < k) or cb == k:
             c += a[i]
             i += 1
+            ca += 1
+            cb = 0
         else:
             c += b[j]
             j += 1
-        op_count += 1
+            cb += 1
+            ca = 0
     return c
 
 
 
-n, m, k = 5, 9, 3
-a, b = "caaca", "bedededeb"
-print(check(n, m, k, a, b))
+# n, m, k = 5, 9, 3
+# a, b = "caaca", "bedededeb"
+
+n, m, k = 7, 7, 1
+a="noskill"
+b="wxhtzdy"
+# print(check(n, m, k, a, b))
 
 
-# t = int(input())
-# for _ in range(t):
-#     n, m, k = map(int, input().split())
-#     a = input()
-#     b = input()
-#     print(check(n, m, k, a, b))
+t = int(input())
+for _ in range(t):
+    n, m, k = map(int, input().split())
+    a = input()
+    b = input()
+    print(check(n, m, k, a, b))
