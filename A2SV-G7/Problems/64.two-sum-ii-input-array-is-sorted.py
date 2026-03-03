42f0069b0284a0f4fd5c1e3df6f745d97dd6bf36
Programming-Sai <saiahprog6@gmail.com>
2026-03-03 18:07:17 +0000

    two-sum-ii-input-array-is-sorted

---
+++ A2SV-G7/Problems/64.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,10 @@
+def twoSum(numbers, target):
+    l, r = 0, len(numbers)-1
+    while l <= r:
+        sum_ = numbers[l] + numbers[r]
+        if sum_ == target:
+            return [l+1, r+1]
+        elif sum_ < target:
+            l += 1
+        else:
+            r -= 1
