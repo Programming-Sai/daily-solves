f9b14b14c07f00ea0003fb989f9a1077bc7b6192
Programming-Sai <saiahprog6@gmail.com>
2026-03-13 13:46:19 +0000

    B_Number_of_Smaller

--- A2SV-G7/Problems/B_Number_of_Smaller.py
+++ A2SV-G7/Problems/B_Number_of_Smaller.py
@@ -1,12 +1,9 @@
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
-res = []
-x, y = 0, 0
-
-while y < m:
-    while x < n and a[x] < b[y]:
-        x += 1
-    res.append(x)
-    y += 1
-print(" ".join([str(c) for c in res]))
+res, l = [], 0
+for num in b:
+    while l < n and a[l] < num:
+        l += 1
+    res.append(l)
+print(*res)
